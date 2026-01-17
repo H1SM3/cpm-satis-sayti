960b2b68199daf30ecacadb4c97e5d682147bdd5
H1SM3 <hsdunt@gmail.com>
2026-01-17 22:04:35 +0400

    Delete data.js

--- data.js
+++
@@ -1,6 +0,0 @@
-var DATA={
-"Valyuta":["50M Pul - 1 AZN","30K Coin - 1 AZN","500K Coin - 2 AZN"],
-"Hesab":["ID dəyişmə - 2 AZN","Boş hesab - 1 AZN","Full hesab (W16 yoxdur) - 8 AZN"],
-"Maşın":["Xrom - 1 AZN","Unikal HP - 1 AZN","Bütün pullu disklər - 1 AZN","Miqalka - 1 AZN"],
-"Əlavə":["Crown almaq - 1 AZN","İstədiyiniz maşını açmaq - 1 AZN","Hesabı full etmək - 4 AZN","1 maşına W16 - 1 MAN"]
-};
